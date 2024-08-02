01ca99edfe4bb6704154402169cf7ffd2e711f36
Nanak <iamnanak01@gmail.com>
2024-08-02 21:55:02 +0530

    Blog Project

---
+++ 12MegaBlogAppwrite/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+    console.log(import.meta.env.VITE_APPWRITE_URL_OR_ENDPOINT)
+  return (
+    <>
+      <h1>Blog App using Appwrite</h1>
+    </>
+  )
+}
+
+export default App

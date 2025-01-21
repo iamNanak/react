30d3fc112e4c017b1c16e64a78bd1c811f1af224
Nanak <iamnanak01@gmail.com>
2025-01-21 22:55:41 +0530

    refactor: enhance styling and structure of Todo components; add usage steps

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,15 +1,15 @@
-import Todos from './components/Todos'
-import AddTodo from './components/AddTodo'
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 function App() {
-  
-
   return (
     <>
-      <h1>Redux Toolkit Todo</h1>
+      <h1 className="flex justify-center items-center text-3xl font-semibold">
+        Redux Toolkit Todo
+      </h1>
       <AddTodo />
       <Todos />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
